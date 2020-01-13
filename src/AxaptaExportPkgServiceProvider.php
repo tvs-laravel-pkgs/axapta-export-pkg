1e14384f3f6a19b4232f966dcaea7fc4e148b17a
Abdul <abdul@uitoux.in>
2020-01-13 11:52:47 +0530

    u

--- src/AxaptaExportPkgServiceProvider.php
+++ src/AxaptaExportPkgServiceProvider.php
@@ -14,6 +14,7 @@ class AxaptaExportPkgServiceProvider extends ServiceProvider {
 		$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 		$this->loadRoutesFrom(__DIR__ . '/routes/api.php');
 		$this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+		$this->loadViewsFrom(__DIR__ . '/views', 'axapta-export-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
 		]);
--- src/public/angular/axapta-export-pkg/pages/axapta-export/controller.js
+++ src/public/angular/axapta-export-pkg/pages/axapta-export/controller.js
@@ -1,3 +1,21 @@
+app.config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider.
+    //CUSTOMER
+    when('/axapta-export-pkg/axapta-export/list', {
+        template: '<axapta-export-list></axapta-export-list>',
+        title: 'AxaptaExports',
+    }).
+    when('/axapta-export-pkg/axapta-export/add', {
+        template: '<axapta-export-form></axapta-export-form>',
+        title: 'Add AxaptaExport',
+    }).
+    when('/axapta-export-pkg/axapta-export/edit/:id', {
+        template: '<axapta-export-form></axapta-export-form>',
+        title: 'Edit AxaptaExport',
+    });
+}]);
+
 app.component('axapta-exportList', {
     templateUrl: axapta - export_list_template_url,
     controller: function($http, $location, HelperService, $scope, $routeParams, $rootScope, $location) {
