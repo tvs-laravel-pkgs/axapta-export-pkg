18a44f3c36a2491ddf299eb4e009abe514ec6985
keerthana-uitoux <keerthana@uitoux.in>
2022-08-23 15:41:59 +0530

    Create HondaAxaptaExport.php

---
+++ src/models/HondaAxaptaExport.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Abs\AxaptaExportPkg;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HondaAxaptaExport extends Model {
+	protected $table = 'honda_axapta_exports';
+	protected $fillable = [
+		 
+		'entity_type_id',
+		'entity_id',
+		'PlantCode',
+		'CurrencyCode',
+		'JournalName',
+		'JournalNum',
+		'LineNum',
+		'Voucher',
+		'ApproverPersonnelNumber',
+		'Approved',
+		'TransDate',
+		'Account',
+		'AccountType',
+		'Department'
+		'LedgerDimension',
+		'DefaultDimension',
+		'Txt',
+		'AmountCurDebit',
+		'AmountCurCredit',
+		'Sub_GL'
+		'OffsetAccountType',
+		'OffsetLedgerDimension',
+		'OffsetDefaultDimension',
+		'PaymMode',
+		'TaxGroup',
+		'TaxItemGroup',
+		'Invoice',
+		'InvoiceDate',
+		'SalesTaxFormTypes_IN_FormType',
+		'TDSGroup_IN',
+		'DocumentNum',
+		'DocumentDate',
+		'DocumentType',
+		'LogisticsLocation_LocationId',
+		'Due',
+		'PaymReference', 
+		'VatPercentage',
+		'Qty',
+	];
+
+}
